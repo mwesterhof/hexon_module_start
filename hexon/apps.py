a513bd521e18e964a916194b019036d1af158f8a
Marco Westerhof <westerhof.marco@gmail.com>
2026-06-24 13:35:05 +0200

    remove hexon app

--- hexon/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HexonConfig(AppConfig):
-    name = 'hexon'
--- hexon/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import HexonMutateView
-
-
-urlpatterns = [
-    path("api/mutate/", HexonMutateView.as_view()),
-]
